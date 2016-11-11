cf97e06b69081c939889779f6fc04a7400e12357
Arun George <arun.george@uconn.edu>
2016-11-10 22:29:00 -0500

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -8,8 +8,8 @@ public class GameOfLife
 {
 	public static void main(String[] args) throws FileNotFoundException 
 	{
-		// will test all 4 examples given by parsing the input file, make a 2D matrix then perform the evolution task
-		for(int i=0; i<4; i++)												
+		// will test all examples given by parsing the input file(s), make a 2D matrix then perform the evolution task
+		for(int i=0; i<args.length; i++)												
 		{
 			String filePath = args[i];
 			ArrayList<ArrayList<Integer>> input = parseInput(filePath);
@@ -25,29 +25,29 @@ public class GameOfLife
 	public static ArrayList<ArrayList<Integer>> parseInput(String filePath) throws FileNotFoundException
 	{
 		/*
-		 * Chose to implement the matrix with ArrayLists because they are dynamic
-		 * and we don't know the dimensions of the matrix ahead of time
+		 * Chose to implement the matrix with ArrayLists because they are dynamic and we don't know the dimensions of the matrix ahead of time.
+		 * The outer data structure will be an ArrayList holding an ArrayList of integers, growing every time there is a new line of input
 		 */
 		
 		ArrayList<ArrayList<Integer>> input = new ArrayList<ArrayList<Integer>>();
-		FileReader fileReader = new FileReader(filePath);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		FileReader fileReader = new FileReader(filePath);						// used to read the file
+		BufferedReader bufferedReader = new BufferedReader(fileReader);			// used to read the file
 		
-		String cellInput;
+		String cellInput;														// will be reading the input line by line, storing into here
 		int col = 0;
 		try
 		{
 			boolean brHasNext = true;
 			while(brHasNext)
 			{
-				cellInput = bufferedReader.readLine();
+				cellInput = bufferedReader.readLine();							// get the next line from the file
 				if(cellInput != null)
 				{
 					input.add(new ArrayList<Integer>());						 
 					for(int i=0; i<cellInput.length(); i++)
 					{
-						int val = ((cellInput.charAt(i)) == 49) ? 1 : 0;		// ASCII for 0 and 1 is 48 and 49 respectively
-						input.get(col).add(val);
+						int val = ((cellInput.charAt(i)) == 49) ? 1 : 0;		// for each line, get each character which we will then need to get it's ASCII file 
+						input.get(col).add(val);								// ASCII for 0 and 1 is 48 and 49 respectively
 					}
 					col++;
 				}
@@ -66,7 +66,7 @@ public class GameOfLife
 		return input;
 	}
 	
-	public static void printGrid(ArrayList<ArrayList<Integer>> input)
+	public static void printGrid(ArrayList<ArrayList<Integer>> input)			// basic function to traverse through every single element of the 2d matrix and print it
 	{
 		for(int i=0; i<input.size(); i++)
 		{
@@ -76,50 +76,60 @@ public class GameOfLife
 		}
 	}
 	
-	private static int countNeighbors(int row, int col, ArrayList<ArrayList<Integer>> grid) 
+	public static int countNeighbors(int row, int col, ArrayList<ArrayList<Integer>> grid) 
 	{
-	    int numNeighbors = 0;		//check all 8 possible neighbors 
+	    int numNeighbors = 0;					//check all 8 possible neighbors 
 
-	    if ((row - 1 >= 0) && (col - 1 >= 0)) 								// check NW
-	        numNeighbors += grid.get(row-1).get(col-1);
-	    if ((row - 1 >= 0) && (col < grid.get(row).size())) 				// check N
-	    	numNeighbors += grid.get(row-1).get(col);
-	    if ((row - 1 >= 0) && (col + 1 < grid.get(row).size())) 			// check NE
-	    	numNeighbors += grid.get(row-1).get(col+1);
-	    if ((row >= 0) && (col - 1 >= 0)) 									// check W
-	    	numNeighbors += grid.get(row).get(col-1);
-	    if ((row < grid.size()) && (col + 1 < grid.get(row).size())) 		// check E
-	    	numNeighbors += grid.get(row).get(col+1);
-	    if ((row + 1 < grid.size()) && (col - 1 >= 0)) 						// check SW
-	    	numNeighbors += grid.get(row+1).get(col-1);
-	    if ((row + 1 < grid.size()) && (col < grid.get(row).size())) 		// check S
-	    	numNeighbors += grid.get(row+1).get(col);;
-	    if ((row + 1 < grid.size()) && (col + 1 < grid.get(row).size())) 	// check SE
-	    	numNeighbors += grid.get(row+1).get(col+1);
+	    int xDir[] = { row-1, row, row+1, 
+	    			   row-1, 	   row+1, 
+	    			   row-1, row, row+1 };		//an array of all the x-coordinates of the surrounding cells
+	    int yDir[] = { col-1, col-1, col-1, 
+	    			   col, 		 col, 
+	    			   col+1, col+1, col+1 };	//an array of all the y-coordinates of the surrounding cells
+	    
+	    for(int i=0; i<8; i++)										// there are a max of 8 neighbors
+	    {
+	    	try
+	    	{
+	    		numNeighbors += grid.get(xDir[i]).get(yDir[i]);		// tries to get a specific adjacent cell using the x and y direction arrays. if the cell is reachable add its value to the count
+	    	}
+	    	catch (IndexOutOfBoundsException e)						// if one of the surrounding cells is out of bounds, thats ok ignore it and continue
+	    	{
+	    		continue;
+	    	}
+	    }
 
 	    return numNeighbors;
 	}
 	
 	public static ArrayList<ArrayList<Integer>> evolve(ArrayList<ArrayList<Integer>> curGrid)
 	{
-		// 2D matrix representing the possible next generation outcome
-		int[][] rule = { {0,0,0,1,0,0,0,0,0} , 		// if (cell == dead) && (aliveNeighbors == 3) then this cell will be alive next gen
-						 {0,0,1,1,0,0,0,0,0} };		// if (cell == alive) && (aliveNeighbors == (2 || 3)) then this cell will be alive next gen
+		/*
+		 *  rule is a 2D matrix representing the next generation's outcome
+		 * 	if (cell == dead) && (aliveNeighbors == 3) then this cell will be alive next gen
+		 * 	if (cell == alive) && (aliveNeighbors == (2 || 3)) then this cell will be alive next gen
+		 */
+
+		
+		// 				how many alive neighbors?
+		//				  0,1,2,3,4,5,6,7,8
+		int[][] rule = { {0,0,0,1,0,0,0,0,0} , 		// this row is if the cell is currently dead. 
+						 {0,0,1,1,0,0,0,0,0} };		// this row is if the cell is currently alive. 
 		
 		int row = curGrid.size();
 		int col = curGrid.get(0).size();
 		
 		// make a new matrix and assign the value for each cell by using the rule matrix above
-		ArrayList<ArrayList<Integer>> nextGrid = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> nextGrid = new ArrayList<ArrayList<Integer>>();	// 2d matrix to represent after one cycle of evolution
 		
 		for(int i=0; i<row; i++)
 		{
 			nextGrid.add(new ArrayList<Integer>());
 			for(int j=0; j<col; j++)
 			{
-				int cell = curGrid.get(i).get(j);
-				int liveNeighbors = countNeighbors(i, j, curGrid);
-				nextGrid.get(i).add(rule[cell][liveNeighbors]);
+				int cell = curGrid.get(i).get(j);							// get the cell from the original grid and check if its alive or not
+				int liveNeighbors = countNeighbors(i, j, curGrid);			// get the number of neighbors for the cell
+				nextGrid.get(i).add(rule[cell][liveNeighbors]);				// get the next stage of that cell using the rule matrix above
 			}
 		}		
 		return nextGrid;
